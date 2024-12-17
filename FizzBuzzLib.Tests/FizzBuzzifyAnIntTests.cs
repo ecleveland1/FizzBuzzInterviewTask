495218181e336b3063fc073c83af8f023e8cfe72
ecleveland1 <clevelanderron@gmail.com>
2024-12-17 16:51:26 -0600

    Update FizzBuzzifyAnIntTests.cs
    Tests for main task.

--- FizzBuzzLib.Tests/FizzBuzzifyAnIntTests.cs
+++ FizzBuzzLib.Tests/FizzBuzzifyAnIntTests.cs
@@ -9,6 +9,29 @@ namespace FizzBuzzLib.Tests
         public void FizzBuzzifyAnInt_WhenPassedAndInt_ReturnsAString()
         {
             // test here
+            // arrange
+            var fizzBuzzIntTest = new FizzBuzz();
+
+            // act
+            var output = fizzBuzzIntTest.FizzBuzzifyAnInt(2);
+
+            //assert
+            output.Should().Be("2");
+
+            output = fizzBuzzIntTest.FizzBuzzifyAnInt(6);
+            output.Should().Be("Fizz");
+
+            output = fizzBuzzIntTest.FizzBuzzifyAnInt(10);
+            output.Should().Be("Buzz");
+
+            output = fizzBuzzIntTest.FizzBuzzifyAnInt(15);
+            output.Should().Be("FizzBuzz");
+
+            output = fizzBuzzIntTest.FizzBuzzValuesForRange(1,3);
+            output = fizzBuzzIntTest.FizzBuzzValuesForRange(9,15);
+            output = fizzBuzzIntTest.FizzBuzzValuesForRange(40,66);
+            output = fizzBuzzIntTest.FizzBuzzValuesForRange(1,100);
+            
         }
     }
 }
